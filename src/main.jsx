ef66906597f375bcd55f01667bd64258ffd028ba
Van Phung <phun0024@algonquinlive.com>
2023-01-24 07:12:56 -0500

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,8 @@ import App from './App'
 import './index.css'
 
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />
-  }
-])
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 )
